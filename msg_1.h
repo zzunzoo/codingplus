44812c46f0ca3cff1c4cabadb8d9a53178809284
aiadmin <zzunzoo@gmail.com>
2020-08-22 18:39:11 +0900

    0822

---
+++ msg_1.h
@@ -0,0 +1,9 @@
+#if defined MAN
+#define PRINT_MSG "성인 남자입니다"
+#elif defined WOMAN
+#define PRINT_MSG "성인 여자입니다"
+#elif defined BOY
+#define PRINT_MSG "소년입니다"
+#elif defined GIRL
+#define PRINT_MSG "소녀입니다"
+#endif
