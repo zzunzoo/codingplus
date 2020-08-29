23d99549ae5d7036aa189983a1b30b0de93631df
aiadmin <zzunzoo@gmail.com>
2020-08-29 17:40:40 +0900

    0829

---
+++ variable4.java
@@ -0,0 +1,14 @@
+class SystemOutPrintln {
+	public static void main(String[] args) {
+		char ch1 = '헐';
+		char ch2 = '확';
+		char ch3 = 54736;
+		char ch4 = 54869;
+		char ch5 = 0xD5D0;
+		char ch6 = 0xD655;
+		System.out.println(ch1 + " " + ch2);
+		System.out.println(ch3 + " " + ch4);
+		System.out.println(ch5 + " " + ch6);
+			
+	}
+}
