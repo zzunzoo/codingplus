4c8e0113747dfa6d4af939d00bd5aeb5f2f16ef9
aiadmin <zzunzoo@gmail.com>
2020-08-29 17:01:56 +0900

    0829

---
+++ println.java
@@ -0,0 +1,11 @@
+class SystemOutPrintln
+{
+	public static void main(String[] args)
+	{
+		System.out.println(7);
+		System.out.println(3.15);
+		System.out.println("3 + 5 = " + 8);
+		System.out.println(3.15 + "는 실수입니다.");
+		System.out.println(3 + 5);
+	}
+}
