522e4d7f0ec9a8f1cef54e18361962f486803d41
aiadmin <zzunzoo@gmail.com>
2020-08-29 18:57:35 +0900

    0829

---
+++ relation.java
@@ -0,0 +1,8 @@
+class SystemOutPrintln {
+	public static void main(String[] args) {
+		System.out.println("3 >= 2 : " + (3 >= 2));
+		System.out.println("3 <= 2 : " + (3 <= 2));
+		System.out.println("7.0 == 7 : " + (7.0 >= 7));
+		System.out.println("7.0 != 7 : " + (7.0 != 7));
+	}
+} 
