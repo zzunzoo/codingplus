f9ffa0a2c05e6a2e09c72354ca0bbc9b2b993974
aiadmin <zzunzoo@gmail.com>
2020-10-10 16:51:19 +0900

    1010

---
+++ condop.java
@@ -0,0 +1,16 @@
+class SystemOutPrintln {
+	public static void main(String[] args) {
+	int num1 = 50;
+	int num2 = 100;
+	int big;
+	int diff;
+	
+	big = (num1 > num2) ? num1 : num2;
+	System.out.println("큰 수 : " + big);
+	
+	diff = (num1 > num2) ? (num1 - num2) : (num2 - num1);
+	System.out.println("절댓값 : " + diff);
+		
+	}
+}
+
