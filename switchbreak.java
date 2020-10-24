b0743b6c40b3d436883222a329c7bf2367342738
aiadmin <zzunzoo@gmail.com>
2020-10-24 17:02:45 +0900

    1024

---
+++ switchbreak.java
@@ -0,0 +1,22 @@
+class SystemOutPrintln {
+	public static void main(String[] args) {
+	int n = 3;
+	
+	switch(n) {
+	case 1:
+		System.out.println("Simple Java");
+		break;
+	case 2:
+		System.out.println("Funny Java");
+		break;
+	case 3:
+		System.out.println("Fantastic Java");
+	default:
+		System.out.println("The best programming language");
+	}
+	
+	System.out.println("Do you like Java?");
+	
+	}
+}
+
