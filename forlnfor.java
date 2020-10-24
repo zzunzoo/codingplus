c3fe4078f4bbdd15b52e6733ea52eaf06a7da535
aiadmin <zzunzoo@gmail.com>
2020-10-24 17:35:23 +0900

    1024

---
+++ forlnfor.java
@@ -0,0 +1,12 @@
+class SystemOutPrintln {
+	public static void main(String[] args) {
+		for(int i = 0; i < 3; i++) {
+			System.out.println("------------------");
+			for(int j = 0; j < 3; j++) {
+				System.out.println("[" + i + ", " + j + "] ");
+			}
+			System.out.println('\n');
+		}
+	}
+}
+
